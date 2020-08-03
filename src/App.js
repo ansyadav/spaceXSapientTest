1a49775b6d034e00802ce58d1c28fd1e42c662a1
anshika <anshika.yadav@magicedtech.com>
2020-08-03 09:54:21 +0530

    code completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import FilterPanel from './components/FilterPanel';
-import './App.css';
 
 function App() {
   return (
--- src/components/FilterPanel.js
+++ src/components/FilterPanel.js
@@ -4,98 +4,85 @@ class FilterPanel extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            // originalDataSaved : [],
             PanelList: [],
-            dataLoad:false,
+            dataLoad:true,
+            year: null,
+            launch: null,
+            land:null,
         }   
     }
     componentDidMount() {
         this.spaceXData();
     }
     spaceXData = () => {
-        fetch('https://api.spacexdata.com/v3/launches?limit=100')
+        const { year , launch, land } = this.state;
+        let url = 'https://api.spacexdata.com/v3/launches?limit=100'
+        if(year !== null) {
+            url = url + `&launch_year=${year}`
+        }
+        if(launch !== null){
+            url = url + `&launch_success=${launch}`
+        } 
+        if(land !== null){
+            url = url + `&land_success==${land}`
+        }
+        fetch(url)
         .then(response => response.json())
         .then(json => {
             this.setState({
                 PanelList: json,
-                // originalDataSaved:json,
-                dataLoad:true,
+                dataLoad:false,
         })
         console.log("PanelList",this.state.PanelList);
         })
     }
     clickOnYearButton = (filterType,getValue) => { 
-        console.log("filterType==",filterType);
-        console.log("getValue==",getValue);
-        if(filterType === 'Y'){
-            // const filteredResultOfYear = this.state.originalDataSaved.filter(name => name.launch_year === getValue);
-            // console.log("clickOnYearButton",filteredResultOfYear);
-            // this.setState({
-            //     PanelList: filteredResultOfYear
-            // })
-            fetch(`https://api.spacexdata.com/v3/launches?limit=100&amp;launch_success=true&amp;land_success=true&amp;launch_year=${getValue}`)
-            .then(response => response.json())
-            .then(json => {
-                this.setState({
-                    PanelList: json,
-            })
-            console.log("PanelList",this.state.PanelList);
-            })
-        }
-        if(filterType === 'LAUNCH'){
-            // const filteredResultOfLaunch = this.state.originalDataSaved.filter(name => name.launch_success === getValue);
-            // console.log("clickOnYearButton",filteredResultOfLaunch);
-            // this.setState({
-            //     PanelList: filteredResultOfLaunch
-            // })
-        }
-        if(filterType === 'LAND'){
-            // const filteredResultOfLand = this.state.originalDataSaved.filter(name => name.land_success === getValue);
-            // console.log("clickOnYearButton",filteredResultOfLand);
-            // this.setState({
-            //     PanelList: filteredResultOfLand
-            // })
-        }
-        
+        this.setState({
+            [filterType] : getValue,
+            dataLoad: true
+        }, () => {
+            this.spaceXData()
+        })
     }
     render() {
-        const { PanelList,dataLoad } = this.state;
-        return<>
-        <main className="main clearfix">
+        const { PanelList,dataLoad ,year , launch , land } = this.state;
+        return<>{
+            dataLoad === false ? 
+            <main className="main clearfix">
             <div className="filter">
                     <h3>Filters</h3>
                     <div className="filterinnerWrap">
                         <span>Launch Year</span>
                         <ul>
-                            <li onClick={() => this.clickOnYearButton('Y','2006')}>2006</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2007')}>2007</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2008')}>2008</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2009')}>2009</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2010')}>2010</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2011')}>2011</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2012')}>2012</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2013')}>2013</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2014')}>2014</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2015')}>2015</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2016')}>2016</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2017')}>2017</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2018')}>2018</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2019')}>2019</li>
-                            <li onClick={() => this.clickOnYearButton('Y','2020')}>2020</li>
+                            <li className={year === 2006 ? "active": ''} onClick={() => this.clickOnYearButton('year',2006)}>2006</li>
+                            <li className={year === 2007 ? "active": ''} onClick={() => this.clickOnYearButton('year',2007)}>2007</li>
+                            <li className={year === 2008 ? "active": ''} onClick={() => this.clickOnYearButton('year',2008)}>2008</li>
+                            <li className={year === 2009 ? "active": ''} onClick={() => this.clickOnYearButton('year',2009)}>2009</li>
+                            <li className={year === 2010 ? "active": ''} onClick={() => this.clickOnYearButton('year',2010)}>2010</li>
+                            <li className={year === 2011 ? "active": ''} onClick={() => this.clickOnYearButton('year',2011)}>2011</li>
+                            <li className={year === 2012 ? "active": ''} onClick={() => this.clickOnYearButton('year',2012)}>2012</li>
+                            <li className={year === 2013 ? "active": ''} onClick={() => this.clickOnYearButton('year',2013)}>2013</li>
+                            <li className={year === 2014 ? "active": ''} onClick={() => this.clickOnYearButton('year',2014)}>2014</li>
+                            <li className={year === 2015 ? "active": ''} onClick={() => this.clickOnYearButton('year',2015)}>2015</li>
+                            <li className={year === 2016 ? "active": ''} onClick={() => this.clickOnYearButton('year',2016)}>2016</li>
+                            <li className={year === 2017 ? "active": ''} onClick={() => this.clickOnYearButton('year',2017)}>2017</li>
+                            <li className={year === 2018 ? "active": ''} onClick={() => this.clickOnYearButton('year',2018)}>2018</li>
+                            <li className={year === 2019 ? "active": ''} onClick={() => this.clickOnYearButton('year',2019)}>2019</li>
+                            <li className={year === 2020 ? "active": ''} onClick={() => this.clickOnYearButton('year',2020)}>2020</li>
                         </ul>
                     </div>
                     <div className="filterinnerWrap">
                         <span>Successful Launch</span>
                         <ul>
-                            <li onClick={() => this.clickOnYearButton('LAUNCH','true')}>true</li>
-                            <li onClick={() => this.clickOnYearButton('LAUNCH','false')}>false</li>
+                            <li  className={launch === true ? "active": ''} onClick={() => this.clickOnYearButton('launch',true)}>true</li>
+                            <li className={launch === false ? "active": ''} onClick={() => this.clickOnYearButton('launch',false)}>false</li>
                         </ul>
                     </div>
                     <div className="filterinnerWrap">
                         <span>Successful Landing</span>
                         <ul>
-                            <li onClick={() => this.clickOnYearButton('LAND','true')}>true</li>
-                            <li onClick={() => this.clickOnYearButton('LAND','false')}>false</li>
+                            <li className={land === true ? "active": ''} onClick={() => this.clickOnYearButton('land',true)}>true</li>
+                            <li className={land === false ? "active": ''} onClick={() => this.clickOnYearButton('land',false)}>false</li>
                         </ul>
                     </div>
             </div>
@@ -113,8 +100,10 @@ class FilterPanel extends React.Component {
                     
                 </ul>
             </div>
-        </main>
-        
+        </main>: <div className="dataLoading"> Data Loading....</div>
+       
+        }
+         
         </>
     }
 }
